df7971e68fb9114babed168359add855c50c7620
Alain Hélaïli <alain@carbonhub.io>
2022-03-21 13:08:51 +0100

    Duration is a parameter

--- index.js
+++ index.js
@@ -1,21 +1,90 @@
+const { createProbot } = require('probot')
+const { exec } = require('child_process')
+const fs = require('fs')
+const express  = require('express')
+const bodyParser = require('body-parser')
+
 /**
- * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
-module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
+module.exports = (app, { getRouter }) => {
+  app.log.info('Starting SSH Certificate Broker')
+  const generatedCerts = {}
+  const certFile = process.env['CERT_FILE']
+  const certDuration = process.env['CERT_DURATION']
+  const router = getRouter('/ssh-cert-app')
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
-  });
+  // Use any middleware
+  router.use(express.static('public'))
+  router.use(bodyParser.json())
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  // Route to send back the generated certificate
+  router.post("/fetch", (req, res) => {
+    app.log.debug(`Looking for certificate for ${req.body.sessionToken}`)
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-};
+    if (generatedCerts[req.body.sessionToken]) {
+      const filenamePrefix = generatedCerts[req.body.sessionToken].pubKeyFileName.split('.')[0]
+      fs.readFile(`${__dirname}/${req.body.sessionToken}/${filenamePrefix}-cert.pub`, 'utf8', (err,data) => {
+        if (err) {
+          app.log.debug(err)
+          res.status(404).send({message: 'Certificate not found'})
+        } else {
+          app.log.debug(data)
+          res.send({certificate: data})
+
+          // Let's clean up memory and disk
+          delete generatedCerts[req.body.sessionToken]
+          fs.rmdir(`${__dirname}/${req.body.sessionToken}`, { recursive: true, force: true }, (err) => {
+            if (err) {
+              app.log.error(err)
+            }
+          })
+        }
+      })
+    } else {
+      res.status(404).send({message: 'Certificate not found'})
+    }
+  })
+
+  app.on("repository_dispatch", async (context) => {
+    app.log.debug({ event: context.name, action: context.payload.action, sender: context.payload.sender.login, payload: context.payload.client_payload })
+    
+    if (!fs.existsSync(`${__dirname}/${context.payload.client_payload.sessionToken}`)) {
+      fs.mkdirSync(`${__dirname}/${context.payload.client_payload.sessionToken}`, (err) => {
+        if (err) {
+          app.log.error(err)
+          return err
+        }
+        app.log.debug(`Directory ${__dirname}/${context.payload.client_payload.sessionToken} is created.`);
+      })
+    }
+  
+    const sshKeyFile = `${__dirname}/${context.payload.client_payload.sessionToken}/${context.payload.client_payload.pubKeyFileName}`
+    fs.writeFileSync(sshKeyFile, context.payload.client_payload.key)
+    fs.close
+
+    // ssh-keygen -s /Users/helaili/.ssh/CA/github-ssh-authority -V '+1d' -I helaili@github.com -O extension:login@github.com=helaili ./id_rsa.pub
+    
+    const args = [
+      '-O', `extension:login@github.com=${context.payload.sender.login}`, 
+      '-s',  certFile, 
+      '-I', context.payload.client_payload.title, 
+      '-n', context.payload.sender.login, 
+      context.payload.client_payload.pubKeyFileName
+    ]
+
+    if (certDuration) {
+      args.unshift('-V', certDuration)
+    }
+
+    // Sign the key
+    const script = exec(`cd ${__dirname}/${context.payload.client_payload.sessionToken} && ssh-keygen ${args.join(' ')}`)
+    script.stdout.on('data', function(data){
+      app.log.debug(data.toString());
+    })
+    script.stderr.on('data', function(data){
+      app.log.debug(data.toString());
+    })
+    generatedCerts[context.payload.client_payload.sessionToken] = context.payload.client_payload
+  })
+}
